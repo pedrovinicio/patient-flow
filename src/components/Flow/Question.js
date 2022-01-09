3f20a2910b1535032cb76242ed8867630472bf99
PEDRO-PC\pedro <pedrovinicio@gmail.com>
2022-01-09 20:26:03 -0300

    Fixing interaction between radio and question

--- src/components/Flow/Question.js
+++ src/components/Flow/Question.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import Button from "../UI/Button";
-import Radio from "../UI/Radio";
 import classes from './Question.module.css';
+import { Wrapper, Item, RadioButton, RadioButtonLabel } from './Radio.styles';
 
 const Question = props => {
     const [answered, setAnswered] = useState(null);
@@ -12,13 +12,28 @@ const Question = props => {
 
     const onNextClicked = () => {
         props.onAnswered(answered);
+        setAnswered(null);
     };
 
     return (
         <div>
             <div className={classes.question}>{props.question.question_text}</div>
             <div className={classes.answersContainer}>
-                <Radio answers={props.question.answers} onChange={onRadioChanged}></Radio>
+                <Wrapper>
+                    {props.question.answers.map((answer) =>(
+                        <Item key={answer.id}>
+                            <RadioButton
+                                type="radio"
+                                name="radio"
+                                value={answer.id}
+                                checked={answered ? (answered.id === answer.id) : false}
+                                onChange={() => onRadioChanged(answer)}
+                            />
+                            <RadioButtonLabel />
+                            <div>{answer.label}</div>
+                        </Item>
+                    ))}
+                </Wrapper>
             </div>
             <div className={classes.buttonContainer}>
                 <Button onClick={onNextClicked} disabled={!answered}>Next</Button>
